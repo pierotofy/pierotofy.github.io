815ddf9794031734e80a567e9d9c269bf16fd4b5
Piero Toffanin <pt@masseranolabs.com>
2016-07-25 17:45:27 -0500

    Added mirror of pokermuck

---
+++ pokermuck/index01df.php
@@ -0,0 +1,16 @@
+; You're seeing this file because you're using Mediawiki's External Editor
+; feature. This is probably because you selected use external editor
+; in your preferences. To edit normally, either disable that preference
+; or go to the URL http://www.pokermuck.com/images/c/c5/Easy-blind-steal-hud.jpg .
+; See http://www.mediawiki.org/wiki/Manual:External_editors for details.
+[Process]
+Type=Edit file
+Engine=MediaWiki
+Script=http://www.pokermuck.com/index.php
+Server=http://www.pokermuck.com
+Path=
+Special namespace=Special
+
+[File]
+Extension=jpg
+URL=http://www.pokermuck.com/images/c/c5/Easy-blind-steal-hud.jpg
---
+++ pokermuck/index890f.php
@@ -0,0 +1,12 @@
+<?xml version="1.0"?>
+<?xml-stylesheet type="text/css" href="http://www.pokermuck.com/skins/common/feed.css?303"?>
+<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en">
+		<id>http://www.pokermuck.com/index.php?title=Special:RecentChanges&amp;feed=atom</id>
+		<title>PokerMuck  - Recent changes [en]</title>
+		<link rel="self" type="application/atom+xml" href="http://www.pokermuck.com/index.php?title=Special:RecentChanges&amp;feed=atom"/>
+		<link rel="alternate" type="text/html" href="http://www.pokermuck.com/index.php/Special:RecentChanges"/>
+		<updated>2016-07-25T22:40:54Z</updated>
+		<subtitle>Track the most recent changes to the wiki in this feed.</subtitle>
+		<generator>MediaWiki 1.18.0</generator>
+
+	</feed>
